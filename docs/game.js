6f756bff797f4477c0f74816b316a5479078261a
naitoyuma3230 <naitoyuma@outlook.jp>
2022-09-09 17:05:48 +0900

    画像もDocsに含める

--- docs/game.js
+++ docs/game.js
@@ -3,9 +3,9 @@ let friend1 = new Friend("ないとう", 999, 1281, 13, 20, 45); // 味方
 let friend2 = new Friend("あかほり", 1, 16, 12, 3, 45); // 味方
 let friend3 = new Friend("とりい", 3, 43, 11, 1, 45); // 味方
 let friend4 = new Friend("やぎ(毒)", 4, 43, 11, 1, 45); // 味方
-let enemy1 = new Troll("トロル", 270, 38, 20, "../image/troll.png"); // 敵
-let enemy2 = new Dragon("ドラゴン", 380, 68, 6, "../image/dragon.png"); // 敵
-let enemy3 = new Kitagawa("北川", 6000, 120, 1, "../image/kitagawa.png"); // 敵
+let enemy1 = new Troll("トロル", 270, 38, 20, "./image/troll.png"); // 敵
+let enemy2 = new Dragon("ドラゴン", 380, 68, 6, "./image/dragon.png"); // 敵
+let enemy3 = new Kitagawa("北川", 6000, 120, 1, "./image/kitagawa.png"); // 敵
 
 // キャラクター配列をつくる
 let characters = [];
